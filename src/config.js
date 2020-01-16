0a8db66eaf6de864d2043e3129df14a37543d88f
adityaprasad97 <prasadaditya97@gmail.com>
2020-01-17 00:19:31 +0530

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notespd-app-upload"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: " https://2lgms07mtl.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_tPdejmFXD",
+        APP_CLIENT_ID: "7mib6q8nqm0sq24mihno4333uo",
+        IDENTITY_POOL_ID: "us-east-1:6a16aa56-9ea4-45a9-8bd2-edd689453bac"
+    }
+};
